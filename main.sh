83ae05ce7df5c4d35c9e318d6383157ddd666c70
marco.tessari <marco.tessari@fv.lan>
2021-05-13 08:44:42 +0200

    added check on number of args input script

--- main.sh
+++ main.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-docker_image=nginx-example
+container_name=nginx-hello-world
+docker_image=nginx-hello-world
 
-echo "building image with name: $docker_image"
+if [ $# -eq 0 ] ; then
+    echo 'No tag image provvided'
+    exit 1
+fi
 
-docker build -t $docker_image .
+image_tag=$1
 
-echo "starting container with name: $docker_image"
+full_image_name=$docker_image:$image_tag
 
-docker run -d --rm --name nginx-example -p 8080:80 nginx-example
+echo "building image with name: $full_image_name"
+
+docker build -t $full_image_name .
+
+echo "starting container with name: $container_name"
+
+docker run -d --rm --name $container_name -p 8080:80 $full_image_name
